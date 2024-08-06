8e059e89720444617e1651c683b65df11f2b1b62
Pavo1034 <kumarpawan963258@gmail.com>
2024-08-07 00:33:51 +0530

    added muted and autoplay

--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -4,7 +4,7 @@ import { imgPoster, video1 } from "./utils/constant";
 const VideoContainer = () => {
   return (
     <div className="w-full pl-20 mt-20">
-      <video width="900" height="500" controls className="rounded-2xl">
+      <video width="900" height="500" autoPlay muted className="rounded-2xl">
         <source src={video1} type="video/mp4" />
       </video>
     </div>
